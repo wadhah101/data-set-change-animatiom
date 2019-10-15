21699f2d0a8d680a60165a0872b208fd1b15132f
boogie <wadhah.mahroug15@gmail.com>
2019-10-15 15:19:38 +0100

    code cleanup

--- animation.py
+++ animation.py
@@ -5,16 +5,18 @@ from multiprocessing import Pool, cpu_count
 
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
-image_folder_name = '.cache'
+pbar = tqdm(total=7200)
 
 
 # todo : offload heavy calculation work to gpu using pyopencl
 def make_frame(i, data_set, delta, frame_rate, duration, limits):
     plt.figure(figsize=(19.20, 10.80))
     plt.ylim(limits[0], limits[1])
+    pbar.update(4)
     plt.plot(x, data_set + (i * 1.0 / (frame_rate * duration)) * delta)
-    plt.savefig('{}/img{:05d}'.format(image_folder_name, i))
+    plt.savefig('.cache/img{:05d}'.format(i))
     plt.close()
 
 
@@ -22,15 +24,14 @@ def make_frame(i, data_set, delta, frame_rate, duration, limits):
 # IT'S NOT ADVISED TO USE ALL AVAILABLE CPUS , THE PC MAY BECOME UNRESPONSIVE
 # todo : add some still frames at the end of the video
 def make_video(data_set, delta, frame_rate, duration):
-    print('Building frames !')
-    if image_folder_name not in os.listdir():
-        os.mkdir(image_folder_name)
-    n_workers = cpu_count() - 1 if cpu_count() > 1 else 1
+    print('Building frames !', flush=True)
+    if '.cache' not in os.listdir():
+        os.mkdir('.cache')
+    n_workers = cpu_count() // 2
     p = Pool(processes=n_workers)
     l1 = min(min(data_set), min(data_set + delta)) * 1.1
     l2 = max(max(data_set), max(data_set + delta)) * 1.1
     limits = (l1, l2)
-
     for i in np.arange(0, frame_rate * duration, 1):
         p.apply_async(make_frame, (i, data_set, delta, frame_rate, duration, limits))
     p.close()
@@ -39,40 +40,34 @@ def make_video(data_set, delta, frame_rate, duration):
     print('Concatenating frames !')
 
     # concatenate frames using ffmpeg
-    commands = 'ffmpeg -y -framerate {} -i {}/img%05d.png -c:v libx265 {}' \
-        .format(frame_rate, image_folder_name, 'vid.mkv') \
-        .split(sep=' ')
+    commands = 'ffmpeg -y -framerate {} -i .cache/img%05d.png -c:v libx265 video.mkv'.format(frame_rate).split()
     null_file = open(os.devnull, 'w')
     child = subprocess.Popen(commands, stderr=null_file)
     child.wait()
 
 
 def check_ffmpeg():
-    commands = ['ffmpeg']
-    null_file = open(os.devnull, 'w')
-    child = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    child = subprocess.Popen(['ffmpeg'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if child.stderr is not None:
         s = child.stderr.readline()
         if not s.startswith(b'ffmpeg version'):
             raise OSError('ffmpeg not found in this environment ')
 
 
-def main(original_data_set, new_data_set, duration=5, frame_rate=60):
+def main(original_data_set, new_data_set, duration=1, frame_rate=60):
     check_ffmpeg()
     delta = new_data_set - original_data_set
     make_video(original_data_set, delta, frame_rate, duration)
     print('Deleting cache files !')
-    shutil.rmtree(image_folder_name)
+    shutil.rmtree('.cache')
     print('Done !')
 
 
 # example
-x = np.arange(1, 10, 0.001)
-o = np.log(x)
-o2 = x * 0
+x = np.arange(-10, 10, 0.01)
+o = np.abs(x)
+o2 = 3 * np.sin(x)
 
 if __name__ == '__main__':
     main(original_data_set=o
-         , new_data_set=o2
-         , duration=5
-         , frame_rate=60)
+         , new_data_set=o2)
